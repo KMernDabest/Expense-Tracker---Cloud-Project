a4bd8c295a2806c8aadde975171ab143b7391cd4
Vorak Tong <viraktong197@gmail.com>
2026-04-09 22:22:32 +0700

    Complete Security Module

---
+++ terraform/modules/security/security.tf
@@ -0,0 +1,130 @@
+###############################################################################
+# VARIABLES
+###############################################################################
+
+variable "project_name" {
+  description = "Project name used for resource naming"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+  type        = string
+}
+
+###############################################################################
+# RESOURCES
+###############################################################################
+
+# ALB Security Group - allows HTTP from the internet
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.project_name}-alb-sg"
+  description = "Allow HTTP traffic to ALB"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "HTTP from internet"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-alb-sg"
+  }
+}
+
+# EC2 Security Group - allows traffic only from ALB
+resource "aws_security_group" "ec2_sg" {
+  name        = "${var.project_name}-ec2-sg"
+  description = "Allow traffic from ALB to EC2 instances"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description     = "HTTP from ALB"
+    from_port       = 3000
+    to_port         = 3000
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS from anywhere"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-ec2-sg"
+  }
+}
+
+# RDS Security Group - allows traffic only from EC2
+resource "aws_security_group" "rds_sg" {
+  name        = "${var.project_name}-rds-sg"
+  description = "Allow PostgreSQL traffic from EC2 instances"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description     = "PostgreSQL from EC2"
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.ec2_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-rds-sg"
+  }
+}
+
+###############################################################################
+# OUTPUTS
+###############################################################################
+
+output "alb_sg_id" {
+  description = "ALB security group ID"
+  value       = aws_security_group.alb_sg.id
+}
+
+output "ec2_sg_id" {
+  description = "EC2 security group ID"
+  value       = aws_security_group.ec2_sg.id
+}
+
+output "rds_sg_id" {
+  description = "RDS security group ID"
+  value       = aws_security_group.rds_sg.id
+}
